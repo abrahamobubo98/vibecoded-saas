bfe94a1ce92352a720106ed97969d4b786076ea2
abrahamdashobubo <abraham.obubo@superbuilders.school>
2025-12-21 21:17:32 -0600

    02: database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,14 @@
 import { PrismaClient, Prisma } from "../src/generated/prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg'
+import 'dotenv/config'
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const prisma = new PrismaClient({
+  adapter,
+});
 
 const userData: Prisma.UserCreateInput[] = [
   {
