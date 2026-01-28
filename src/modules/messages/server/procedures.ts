0a98418d178254f4061e0c53d2e2fd711b022f5d
abrahamobubo98 <abrahamobubo@gmail.com>
2026-01-28 00:23:24 -0600

    08: messages

---
+++ src/modules/messages/server/procedures.ts
@@ -0,0 +1,45 @@
+import { z } from "zod";
+import { inngest } from "@/inngest/client";
+
+import prisma from "@/lib/db";
+import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+
+export const messagesRouter = createTRPCRouter({
+    getMany: baseProcedure
+        .query(async () => {
+            const messages = await prisma.message.findMany({
+                orderBy: {
+                    updatedAt: "desc",
+                },
+                include: {
+                    fragment: true,
+                },
+            });
+
+            return messages;
+        }),
+    create: baseProcedure
+        .input(
+            z.object({
+                value: z.string().min(1, { message: "Message is required" }),
+            }),
+        )
+        .mutation(async ({ input }) => {
+            const createdMessage = await prisma.message.create({
+                data: {
+                    content: input.value,
+                    role: "USER",
+                    type: "RESULT",
+                },
+            });
+
+            await inngest.send({
+                name: "code-agent/run",
+                data: { value: input.value },
+            });
+
+            return createdMessage;
+        }),
+});
+
+export default messagesRouter;
