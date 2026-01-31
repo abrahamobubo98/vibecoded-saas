0660a892e0f7b5016bbdf940945b4998f7c2d01e
abrahamobubo98 <abrahamobubo@gmail.com>
2026-01-31 05:20:54 -0600

    16: Authentication

--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -1,21 +1,21 @@
 import { z } from "zod";
-import { inngest } from "@/inngest/client";
+import { generateSlug } from "random-word-slugs";
 
 import prisma from "@/lib/db";
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
-
-import { generateSlug } from "random-word-slugs";
 import { TRPCError } from "@trpc/server";
+import { inngest } from "@/inngest/client";
+import { protectedProcedure, createTRPCRouter } from "@/trpc/init";
 
 export const projectsRouter = createTRPCRouter({
-    getOne: baseProcedure
+    getOne: protectedProcedure
         .input(z.object({
             id: z.string().min(1, { message: "Id is required" }),
         }))
-        .query(async ({ input }) => {
+        .query(async ({ input, ctx }) => {
             const existingProject = await prisma.project.findUnique({
                 where: {
                     id: input.id,
+                    userId: ctx.auth.userId,
                 },
             });
 
@@ -25,9 +25,12 @@ export const projectsRouter = createTRPCRouter({
 
             return existingProject;
         }),
-    getMany: baseProcedure
-        .query(async () => {
+    getMany: protectedProcedure
+        .query(async ({ ctx }) => {
             const projects = await prisma.project.findMany({
+                where: {
+                    userId: ctx.auth.userId,
+                },
                 orderBy: {
                     updatedAt: "desc",
                 },
@@ -35,17 +38,18 @@ export const projectsRouter = createTRPCRouter({
 
             return projects;
         }),
-    create: baseProcedure
+    create: protectedProcedure
         .input(
             z.object({
                 value: z.string()
-                    .min(1, { message: "Prompt is required" })
-                    .max(10000, { message: "Prompt is too long" }),
+                    .min(1, { message: "Value is required" })
+                    .max(10000, { message: "Value is too long" })
             }),
         )
-        .mutation(async ({ input }) => {
+        .mutation(async ({ input, ctx }) => {
             const createdProject = await prisma.project.create({
                 data: {
+                    userId: ctx.auth.userId,
                     name: generateSlug(2, {
                         format: "kebab",
                     }),
@@ -70,5 +74,3 @@ export const projectsRouter = createTRPCRouter({
             return createdProject;
         }),
 });
-
-export default projectsRouter;
